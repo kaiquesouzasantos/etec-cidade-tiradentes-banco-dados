e4f3613046907e7c63fc2592c2656db4c67d28b1
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-03-30 11:21:31 -0300

    Add files via upload

---
+++ 2022/BD(30-03-2022)/BD(30-03-2022)-Select.sql
@@ -0,0 +1,37 @@
+USE bdEstoque
+
+-- exercicio 01
+INSERT INTO tbFornecedor(nomeFornecedor)
+VALUES 
+	('Coca-Cola')
+	,('Pepsico')
+	,('Heineken');
+
+-- exercicio 02
+SELECT tbFornecedor.nomeFornecedor AS 'FORNECEDOR', tbProduto.descricaoProduto AS 'DESC' FROM tbFornecedor 
+INNER JOIN tbProduto ON tbProduto.codFornecedor = tbFornecedor.codFornecedor
+
+-- exercicio 03
+SELECT tbFornecedor.nomeFornecedor AS 'FORNECEDOR', tbProduto.descricaoProduto AS 'DESC' FROM tbFornecedor 
+LEFT JOIN tbProduto ON tbProduto.codFornecedor = tbFornecedor.codFornecedor
+
+SELECT tbFornecedor.nomeFornecedor AS 'FORNECEDOR', tbProduto.descricaoProduto AS 'DESC' FROM tbProduto 
+RIGHT JOIN tbFornecedor ON tbProduto.codFornecedor = tbFornecedor.codFornecedor
+
+-- exercicio 04
+INSERT INTO tbProduto(descricaoProduto, valorProduto, quantidadeProduto)
+VALUES 
+	('Coca-Cola 2 litros', 6.0, 1500)
+	,('Pepsi', 5.0, 800)
+	,('Cerveja 600ml', 10.5, 28111);
+
+-- exercicio 05
+SELECT tbFornecedor.nomeFornecedor AS 'FORNECEDOR',  tbProduto.descricaoProduto AS 'PRODUTO' FROM tbFornecedor
+FULL JOIN tbProduto ON tbProduto.codFornecedor = tbFornecedor.codFornecedor
+
+SELECT tbProduto.descricaoProduto AS 'PRODUTO', tbFornecedor.nomeFornecedor AS 'FORNECEDOR' FROM tbProduto
+FULL JOIN tbFornecedor ON tbProduto.codFornecedor = tbFornecedor.codFornecedor
+
+-- exercicio 06
+SELECT tbFornecedor.nomeFornecedor, tbProduto.descricaoProduto FROM tbProduto 
+CROSS JOIN tbFornecedor
