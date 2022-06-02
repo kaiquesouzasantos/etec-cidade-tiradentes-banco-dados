237f7c341e577d2bd931ddcdf533100f2fc8afa7
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-02 04:16:39 -0300

    Add files via upload

---
+++ 2022/bdEscola/BD(11-05-2022)-View.sql
@@ -0,0 +1,65 @@
+﻿USE bdEscola
+GO
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- Exericicio 01
+-- Crie uma visão “Preço_Baixo” que exiba o código, nome do curso, carga horária e o valor do curso de todos os cursos que tenha preço inferior ao preço médio.​
+
+CREATE VIEW vwPreco_Baixo AS 
+	SELECT codCurso AS 'ID', nomeCurso AS 'CURSO', cargaHorariaCurso AS 'CARGA_HORARIA', valorCurso AS 'VALOR' FROM tbCurso 
+	WHERE valorCurso < (SELECT AVG(valorCurso) FROM tbCurso)
+GO
+SELECT * FROM vwPreco_Baixo
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- Exercicio 02
+-- Usando a visão “Preço_Baixo”, mostre todos os cursos ordenados por carga horária.​
+
+SELECT [CURSO] FROM vwPreco_Baixo3 ORDER BY [CARGA_HORARIA] ASC
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- Exercicico 03
+-- Crie uma visão “Alunos_Turma” que exiba o curso e a quantidade de alunos por turma.​
+
+CREATE VIEW vwAlunos_Turma AS
+	SELECT nomeCurso AS 'CURSO', COUNT(tbMatricula.codMatricula) AS 'ALUNOS' FROM tbCurso
+		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
+		INNER JOIN tbMatricula ON tbMatricula.codTurma = tbTurma.codTurma
+		GROUP BY nomeCurso
+GO
+SELECT * FROM vwAlunos_Turma 
+
+-- solucao alternativa:
+CREATE VIEW vwAlunos_Turma_Alternativa AS
+	SELECT nomeTurma AS 'TURMA', COUNT(tbMatricula.codMatricula) AS 'ALUNOS' FROM tbCurso
+		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
+		INNER JOIN tbMatricula ON tbMatricula.codTurma = tbTurma.codTurma
+		GROUP BY nomeTurma
+GO
+SELECT * FROM vwAlunos_Turma_Alternativa 
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- Exercicio 04
+-- Usando a visão “Alunos_Turma” exiba a turma com maior número de alunos.​
+
+SELECT MAX([CURSO]) AS 'CURSO', MAX([ALUNOS]) AS 'ALUNOS' FROM vwAlunos_Turma  
+
+-- solucao altenativa correspondente:
+SELECT MAX([TURMA]) AS 'CURSO', MAX([ALUNOS]) AS 'ALUNOS' FROM vwAlunos_Turma_Alternativa
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- Exercicio 05
+-- Crie uma visão “Turma_Curso que exiba o curso e a quantidade de turmas.​
+
+CREATE VIEW vwTurma_Curso AS
+	SELECT nomeCurso AS 'CURSO', COUNT(tbTurma.codTurma) AS 'TURMA' FROM tbCurso
+		INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
+		GROUP BY nomeCurso
+GO
+SELECT * FROM vwTurma_Curso
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- Exercicio 06
+-- Usando a visão “Turma_Curso exiba o curso com menor número de turmas.​
+
+SELECT MIN([CURSO]) AS 'CURSO', MIN([TURMA]) AS 'TURMA' FROM vwTurma_Curso
---
+++ 2022/bdEscolaIdiomas/BD(04-05-2022)-Query.sql
@@ -0,0 +1,79 @@
+USE bdEscolaIdiomas
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 01
+-- Criar uma consulta que retorne o nome e o preço dos cursos que custem abaixo do valor médio.
+
+SELECT nomeCurso AS 'CURSO', valorCurso AS 'VALOR' FROM tbCurso
+WHERE valorCurso < (SELECT AVG(valorCurso) FROM tbCurso)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 02
+-- Criar uma consulta que retorne o nome e o rg do aluno mais novo.
+
+SELECT nomeAluno AS 'ALUNO', rgAluno AS 'RG' FROM tbAluno
+WHERE dataNascAluno = (SELECT MAX(dataNascAluno) FROM tbAluno)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 03
+-- Criar uma consulta que retorne o nome do aluno mais velho.
+
+SELECT nomeAluno AS 'ALUNO' FROM tbAluno
+WHERE dataNascAluno = (SELECT MIN(dataNascAluno) FROM tbAluno)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 04
+-- Criar uma consulta que retorne o nome e o valor do curso mais caro.
+
+SELECT nomeCurso AS 'CURSO', valorCurso AS 'VALOR' FROM tbCurso
+WHERE valorCurso = (SELECT MAX(valorCurso) FROM tbCurso)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 05
+-- Criar uma consulta que retorne o nome do aluno e o nome do curso, do aluno que fez a última matrícula.
+
+-- com INNER JOIN:
+SELECT nomeAluno AS 'ALUNO', nomeCurso AS 'CURSO' FROM tbAluno
+INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
+INNER JOIN tbTurma ON tbTurma.codTurma = tbMatricula.codTurma
+INNER JOIN tbCurso ON tbCurso.codCurso = tbTurma.codCurso
+WHERE dataMatricula = (SELECT MAX(dataMatricula) FROM tbMatricula)
+
+-- sem INNER JOIN:
+SELECT nomeAluno AS 'ALUNO', nomeCurso AS 'CURSO' FROM tbAluno, tbMatricula, tbTurma, tbCurso
+WHERE tbMatricula.codAluno = tbAluno.codAluno
+	AND tbTurma.codTurma = tbMatricula.codTurma
+	AND tbCurso.codCurso = tbTurma.codCurso
+	AND dataMatricula = (SELECT MAX(dataMatricula) FROM tbMatricula)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 06
+-- Criar uma consulta que retorne o nome do primeiro aluno a ser matriculado na escola de Idiomas.
+
+-- com INNER JOIN:
+SELECT nomeAluno AS 'ALUNO' FROM tbAluno
+INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
+WHERE dataMatricula = (SELECT MIN(dataMatricula) FROM tbMatricula)
+
+-- sem INNER JOIN:
+SELECT nomeAluno AS 'ALUNO' FROM tbAluno, tbMatricula
+WHERE tbMatricula.codAluno = tbAluno.codAluno 
+	AND dataMatricula = (SELECT MIN(dataMatricula) FROM tbMatricula)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- exercicio 07
+-- Criar uma consulta que retorne o nome, rg e data de nascimento de todos os alunos que estejam matriculados no curso de inglês.
+
+-- com INNER JOIN:
+SELECT nomeAluno AS 'ALUNO', rgAluno AS 'RG', dataNascAluno AS 'NASCIMENTO' FROM tbAluno
+INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
+INNER JOIN tbTurma ON tbTurma.codTurma = tbMatricula.codTurma
+INNER JOIN tbCurso ON tbCurso.codCurso = tbTurma.codCurso
+WHERE nomeCurso = (SELECT nomeCurso FROM tbCurso WHERE nomeCurso LIKE 'Inglês')
+
+-- sem INNER JOIN:
+SELECT nomeAluno AS 'ALUNO', rgAluno AS 'RG', dataNascAluno AS 'NASCIMENTO' FROM tbAluno, tbCurso, tbMatricula, tbTurma
+WHERE tbMatricula.codAluno = tbAluno.codAluno 
+	AND tbTurma.codTurma = tbMatricula.codTurma 
+	AND tbCurso.codCurso = tbTurma.codCurso
+	AND nomeCurso = (SELECT nomeCurso FROM tbCurso WHERE nomeCurso LIKE 'Inglês')
