d95df0fae90b53cfbe8b18865c7e4e432d9a44fd
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-27 22:45:41 -0300

    Add files via upload

---
+++ 2021/BD (05_11_2021)-Brasileira.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE bdBrasileira
+GO
+USE bdBrasileira
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- CREATE'S
+
+CREATE TABLE tb_autor(
+	codAutor INT PRIMARY KEY NOT NULL
+	,nomeAutor VARCHAR(100)
+);
+GO
+
+CREATE TABLE tb_livro(
+	codLivro INT PRIMARY KEY NOT NULL
+	,nomeLivro VARCHAR(100)
+	,codAutor INT FOREIGN KEY REFERENCES tb_autor(codAutor)
+	,isbnLivro INT
+	,editoraLivro VARCHAR(100)
+	,generoLivro VARCHAR(100)
+	,numPagina INT
+);
+GO
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- INSERT'S
+
+INSERT INTO tb_autor(codAutor,nomeAutor) VALUES
+	(1,'Carlos D. de Andrade'),
+	(2,'Vinicius de Moraes'),
+	(3,'Cecília Meirelles'),
+	(4,'Manuel Bandeira'),
+	(5,'Machado de Asis'),
+	(6,'Cora Coralina'),
+	(7,'Clarice Lispector');
+GO
+
+INSERT INTO tb_livro VALUES
+	(1,'Estórias da casa velha da ponte',6,12438739,'Ática','Romance',298),
+	(2,'O tesoura da Casa velha',6,63762892,'Cia das Letras','Romance',172),
+	(3,'A hora da Estrela',7,73682920,'Cia das Letras','Poesia',92),
+	(4,'Meu livro de cordel',6,72879012,'Saraiva','Poesia',123),
+	(5,'A descoberta do mundo',7,98920212,'Saraiva','Romance',321),
+	(6,'Ou isto ou aquilo',3,73928923,'Ática','Poesia',122);
+GO
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- PROPOSTA DE EXERCICIO
+
+UPDATE tb_autor SET nomeAutor='Carlos Drummnond de Andrade' WHERE nomeAutor='Carlos D. de Andrade';
+
+DELETE tb_livro WHERE isbnLivro = 72879012; 
+DELETE tb_livro WHERE isbnLivro = 98920212; 
+
+CREATE PROCEDURE procTables AS BEGIN SELECT * FROM tb_livro SELECT * FROM tb_autor END
+EXEC procTables
