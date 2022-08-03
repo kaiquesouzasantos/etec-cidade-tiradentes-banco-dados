21fc270579bb89de79d3185b0a5b89d6c73096dd
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-08-03 19:15:43 -0300

    Add files via upload

---
+++ 2022/bdEscola/BD(03-08-2022)-Procedure.sql
@@ -0,0 +1,117 @@
+USE bdEscola
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+ -- 1) Criar uma stored procedure �Busca_Aluno� que receba o c�digo do aluno e retorne seu nome e data de nascimento.
+
+CREATE PROCEDURE spBusca_ALuno
+	@codALuno INT
+	AS BEGIN
+		IF EXISTS(SELECT codAluno FROM tbAluno WHERE codAluno = @codALuno) BEGIN
+			SELECT nomeAluno AS 'NOME', dataNascAluno AS 'NASCIMENTO' FROM tbAluno 
+				WHERE codAluno = @codALuno
+		END
+		ELSE BEGIN
+			PRINT('DADO INEXISTENTE!')
+		END
+	END
+GO
+EXEC spBusca_ALuno 2
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- 2) Criar uma stored procedure Insere_Aluno que insira um registro na tabela de Alunos.
+
+CREATE PROCEDURE sp_Insere_Aluno
+	@nomeAluno VARCHAR(50), @dataNascAluno SMALLDATETIME
+	,@rgAluno VARCHAR(15), @naturalidade VARCHAR(30)
+	AS BEGIN
+		IF EXISTS(SELECT rgAluno FROM tbAluno WHERE rgAluno LIKE @rgAluno) BEGIN 
+			PRINT('DADO JA EXISTENTE!')
+		END
+		ELSE BEGIN
+			INSERT INTO tbAluno(nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)
+				VALUES 
+					(@nomeAluno, @dataNascAluno, @rgAluno, @naturalidade)
+			PRINT('CADASTRO EFETUADO COM SUCESSO')
+		END
+	END
+GO
+EXEC sp_Insere_Aluno 'Kaique', '03-01-2022', '15.552.478-90', 'Brasil'
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- 3) Criar uma stored procedure Aumenta_Preco que, dados o nome do curso e um percentual, aumente o valor do curso com a porcentagem informada.
+
+CREATE PROCEDURE spAumenta_Preco
+	@nomeCurso VARCHAR(50), @percentual DECIMAL
+	AS BEGIN
+		IF EXISTS(SELECT nomeCurso FROM tbCurso WHERE nomeCurso = @nomeCurso) BEGIN
+			UPDATE tbCurso SET valorCurso = valorCurso + valorCurso*@percentual WHERE nomeCurso = @nomeCurso
+		END
+		ELSE BEGIN
+			PRINT('DADO INEXISTENTE!')
+		END
+	END
+GO
+EXEC spAumenta_Preco 'Ingles', 25
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- 4) Criar uma stored procedure �Exibe_Turma� que, dado o nome da turma exiba todas as informa��es dela.
+
+CREATE PROCEDURE spExibe_Turma 
+	@nomeTurma VARCHAR(30)
+	AS BEGIN
+		IF EXISTS(SELECT nomeTurma FROM tbTurma WHERE nomeTurma = @nomeTurma) BEGIN
+			SELECT * FROM tbTurma WHERE nomeTurma = @nomeTurma
+		END
+		ELSE BEGIN
+			PRINT('DADO INEXISTENTE!')
+		END
+	END
+GO
+EXEC spExibe_Turma '1AA'
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- 5) Criar uma stored procedure Exibe_AlunosdaTurma que, dado o nome da turma exiba os seus alunos.
+
+CREATE PROCEDURE spExibe_AlunosTurma
+	@nomeTurma VARCHAR(30)
+	AS BEGIN
+		IF EXISTS(SELECT nomeTurma FROM tbTurma WHERE nomeTurma = @nomeTurma) BEGIN
+			SELECT tbAluno.nomeAluno FROM tbAluno, tbMatricula, tbTurma 
+				WHERE tbMatricula.codAluno = tbAluno.codAluno 
+					AND tbTurma.codTurma = tbMatricula.codTurma AND tbTurma.nomeTurma = @nomeTurma;
+		END
+		ELSE BEGIN
+			PRINT('DADO INEXISTENTE!')
+		END
+	END
+GO
+EXEC spExibe_AlunosTurma '1AA'
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+-- 6) Criar uma stored procedure que receba o nome do curso e o nome do aluno e matricule o mesmo no curso pretendido.
+
+CREATE PROCEDURE sp_Insere_Matricula
+	@nomeCurso VARCHAR(50), @nomeAluno VARCHAR(50)
+	AS BEGIN
+		IF EXISTS(SELECT nomeCurso, nomeAluno FROM tbCurso, tbAluno WHERE nomeCurso = @nomeCurso AND nomeAluno LIKE @nomeAluno) BEGIN
+			DECLARE @codTurma INT
+			DECLARE @codAluno INT
+
+			SET @codTurma = (
+				SELECT MAX(codTurma) FROM tbTurma
+					INNER JOIN tbCurso ON tbCurso.codCurso = tbTurma.codCurso
+					WHERE nomeCurso LIKE @nomeCurso
+			)
+
+			SET @codAluno = (
+				SELECT codAluno FROM tbAluno WHERE nomeAluno LIKE @nomeAluno
+			)
+
+			INSERT INTO tbMatricula(dataMatricula, codAluno, codTurma) VALUES
+				(GETDATE(), @codAluno, @codTurma)
+		END
+		ELSE BEGIN
+			PRINT('PARAMETROS INVALIDOS!')
+		END
+	END
+GO
+EXEC sp_Insere_Matricula 'Inglês', 'Kaique'
