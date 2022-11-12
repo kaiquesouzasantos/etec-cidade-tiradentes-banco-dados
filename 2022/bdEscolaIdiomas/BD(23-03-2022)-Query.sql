2311e6a3e9351a4c41076e1e012fcc0150167363
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-11-12 16:39:56 -0300

    Update BD(23-03-2022)-Query.sql

--- 2022/bdEscolaIdiomas/BD(23-03-2022)-Query.sql
+++ 2022/bdEscolaIdiomas/BD(23-03-2022)-Query.sql
@@ -1,8 +1,7 @@
-﻿USE dbEscolaIdiomas
+USE dbEscolaIdiomas
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
- -- Exercicio 01 
- -- Apresentar os nomes dos alunos ao lado do nome dos cursos que eles fazem;
+ -- 01) Apresentar os nomes dos alunos ao lado do nome dos cursos que eles fazem;
  
  SELECT nomeAluno AS ALUNO, nomeCurso AS CURSO FROM tbAluno 
     INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
@@ -10,8 +9,7 @@
     INNER JOIN tbCurso ON tbCurso.codCurso = tbTurma.codCurso
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 02
--- Apresentar a quantidade de alunos matriculados por nome do curso;
+-- 02) Apresentar a quantidade de alunos matriculados por nome do curso;
  
 SELECT nomeCurso AS CURSO, COUNT(tbAluno.codAluno) AS QTD_MATRICULADOS FROM tbCurso
     INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
@@ -20,8 +18,7 @@ SELECT nomeCurso AS CURSO, COUNT(tbAluno.codAluno) AS QTD_MATRICULADOS FROM tbCu
     GROUP BY nomeCurso
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 03
--- Apresentar a quantidade de alunos matriculados por nome da turma;
+-- 03) Apresentar a quantidade de alunos matriculados por nome da turma;
 
 SELECT DISTINCT nomeTurma AS TURMA, COUNT(tbAluno.nomeAluno) AS QTD_ALUNOS FROM tbAluno
     INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
@@ -29,8 +26,7 @@ SELECT DISTINCT nomeTurma AS TURMA, COUNT(tbAluno.nomeAluno) AS QTD_ALUNOS FROM 
     GROUP BY nomeTurma
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 04
--- Apresentar a quantidade de alunos que fizeram matricula em maio de 2016;
+-- 04) Apresentar a quantidade de alunos que fizeram matricula em maio de 2016;
 
 SELECT COUNT(tbAluno.codAluno) AS QTD_ALUNOS FROM tbAluno
     INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
@@ -38,8 +34,7 @@ SELECT COUNT(tbAluno.codAluno) AS QTD_ALUNOS FROM tbAluno
         AND YEAR(tbMatricula.dataMatricula) = 2016
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 05
--- Apresentar o nome dos alunos em ordem alfabética ao lado do nome das turmas em que estão matriculados;
+-- 05) Apresentar o nome dos alunos em ordem alfabética ao lado do nome das turmas em que estão matriculados;
 
  SELECT nomeAluno AS ALUNO, nomeTurma AS TURMA FROM tbAluno 
     INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
@@ -47,29 +42,25 @@ SELECT COUNT(tbAluno.codAluno) AS QTD_ALUNOS FROM tbAluno
     ORDER BY nomeAluno ASC
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 06
--- Apresentar o nome dos cursos e os horários em que eles são oferecidos;
+-- 06) Apresentar o nome dos cursos e os horários em que eles são oferecidos;
 
 SELECT nomeCurso AS CURSO, horarioTurma AS HORARIO FROM tbCurso
     INNER JOIN tbTurma ON tbTurma.codCurso = tbCurso.codCurso
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 07
--- Apresentar a quantidade de alunos nascidos por estado;
+-- 07) Apresentar a quantidade de alunos nascidos por estado;
 
 SELECT naturalidadeAluno AS ESTADO, COUNT(tbAluno.codAluno) AS QTD_ALUNOS FROM tbAluno
     GROUP BY naturalidadeAluno
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 08
--- Apresentar o nome dos alunos ao lado da data de matrícula no formato dd/mm/aaaa;
+-- 08) Apresentar o nome dos alunos ao lado da data de matrícula no formato dd/mm/aaaa;
 
 SELECT nomeAluno AS ALUNO, CONVERT(VARCHAR, dataMatricula,103) AS DATA_MATRICULA FROM tbAluno
     INNER JOIN tbMatricula ON tbMatricula.codAluno = tbAluno.codAluno
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 09
--- Apresentar os alunos cujo nome comece com A e que estejam matriculados no curso de Ingles;
+-- 09) Apresentar os alunos cujo nome comece com A e que estejam matriculados no curso de Ingles;
 
 SELECT nomeAluno AS ALUNO FROM tbAluno
     INNER JOIN tbMatricula ON tbMatricula.codAluno =  tbAluno.codAluno
@@ -78,8 +69,7 @@ SELECT nomeAluno AS ALUNO FROM tbAluno
     WHERE nomeAluno LIKE 'A%' AND nomeCurso LIKE 'Ingles'
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 10
--- Apresentar a quantidade de matriculas feitas no ano de 2016;
+-- 10) Apresentar a quantidade de matriculas feitas no ano de 2016;
 
 SELECT COUNT(tbMatricula.codMatricula) AS QTD_MATRICULAS FROM tbMatricula 
     WHERE YEAR(tbMatricula.dataMatricula) = 2016
