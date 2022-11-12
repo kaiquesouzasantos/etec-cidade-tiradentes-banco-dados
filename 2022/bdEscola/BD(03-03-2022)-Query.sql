275a359baeba6cd2e352cc7cf606712f69693488
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-11-12 16:38:07 -0300

    Update BD(03-03-2022)-Query.sql

--- 2022/bdEscola/BD(03-03-2022)-Query.sql
+++ 2022/bdEscola/BD(03-03-2022)-Query.sql
@@ -1,54 +1,54 @@
  USE bdEscola
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 01 
+-- 01) Listar o nome, rg e data de aniversário dos alunos nascidos em SP
  
 SELECT nomeAluno AS NOME,rgAluno AS RG,dataNascAluno AS NASCIMENTO FROM tbAluno WHERE naturalidadeAluno = 'SP';
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 02
+-- 02) Listar o nome e o rg dos alunos com o nome começado com P
  
 SELECT  nomeAluno AS NOME,rgAluno AS RG FROM tbAluno WHERE nomeAluno LIKE 'P%';
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 03
+-- 03) Listar o nome dos cursos cuja carga horária seja superior a 2000 horas
 
 SELECT nomeCurso AS CURSO FROM tbCurso WHERE cargaHorariaCurso > 2000;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 04
+-- 04) Listar o nome e o rg de todos os alunos que possuem o sobrenome Silva
 
 SELECT nomeAluno AS NOME,rgAluno AS RG FROM tbAluno WHERE nomeAluno LIKE '%Silva%';
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 05
+-- 05) Listar o nome dos alunos e a data de nascimento que fazem aniversário em março
 
 SELECT nomeAluno AS NOME,dataNascAluno AS NASCIMENTO FROM tbAluno WHERE MONTH(dataNascAluno) = 3;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 06
+-- 06) Listar o código dos alunos e a data de matrícula dos alunos matriculados em maio de qualquer ano
 
 SELECT tbAluno.codAluno AS CODIGO, tbMatricula.dataMatricula AS DATA_MATRICULA FROM tbAluno, tbMatricula 
     WHERE tbAluno.codAluno = tbMatricula.codAluno AND MONTH(tbMatricula.dataMatricula) = 5;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 07
+-- 07) Listar o código dos alunos matriculados no curso de inglês
 
 SELECT tbAluno.codAluno AS CODIGO FROM tbAluno, tbMatricula, tbTurma 
     WHERE tbMatricula.codAluno = tbAluno.codAluno 
         AND tbTurma.codTurma = tbMatricula.codTurma AND tbTurma.codCurso = 1;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 08
+-- 08) Listar o código dos alunos matriculados na turma 1AA
 
 SELECT codAluno AS ID_ALUNO FROM tbMatricula WHERE codTurma = 3;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 09
+-- 09) Listar todos os dados de todos os alunos
 
 SELECT * FROM tbAluno;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- Exercicio 10
+-- 10) Listar todos os dados de todas as turmas
 
 SELECT * FROM tbTurma;
