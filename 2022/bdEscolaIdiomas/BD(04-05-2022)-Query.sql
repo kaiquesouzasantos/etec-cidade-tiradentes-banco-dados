8c4fb15929bde466511223d1a0a4048b74321f7f
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-11-12 16:41:08 -0300

    Update BD(04-05-2022)-Query.sql

--- 2022/bdEscolaIdiomas/BD(04-05-2022)-Query.sql
+++ 2022/bdEscolaIdiomas/BD(04-05-2022)-Query.sql
@@ -1,36 +1,31 @@
 USE bdEscolaIdiomas
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 01
--- Criar uma consulta que retorne o nome e o preço dos cursos que custem abaixo do valor médio.
+-- 01) Criar uma consulta que retorne o nome e o preço dos cursos que custem abaixo do valor médio.
 
 SELECT nomeCurso AS 'CURSO', valorCurso AS 'VALOR' FROM tbCurso
 	WHERE valorCurso < (SELECT AVG(valorCurso) FROM tbCurso)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 02
--- Criar uma consulta que retorne o nome e o rg do aluno mais novo.
+-- 02) Criar uma consulta que retorne o nome e o rg do aluno mais novo.
 
 SELECT nomeAluno AS 'ALUNO', rgAluno AS 'RG' FROM tbAluno
 	WHERE dataNascAluno = (SELECT MAX(dataNascAluno) FROM tbAluno)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 03
--- Criar uma consulta que retorne o nome do aluno mais velho.
+-- 03) Criar uma consulta que retorne o nome do aluno mais velho.
 
 SELECT nomeAluno AS 'ALUNO' FROM tbAluno
 	WHERE dataNascAluno = (SELECT MIN(dataNascAluno) FROM tbAluno)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 04
--- Criar uma consulta que retorne o nome e o valor do curso mais caro.
+-- 04) Criar uma consulta que retorne o nome e o valor do curso mais caro.
 
 SELECT nomeCurso AS 'CURSO', valorCurso AS 'VALOR' FROM tbCurso
 	WHERE valorCurso = (SELECT MAX(valorCurso) FROM tbCurso)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 05
--- Criar uma consulta que retorne o nome do aluno e o nome do curso, do aluno que fez a última matrícula.
+-- 05) Criar uma consulta que retorne o nome do aluno e o nome do curso, do aluno que fez a última matrícula.
 
 -- com INNER JOIN:
 SELECT nomeAluno AS 'ALUNO', nomeCurso AS 'CURSO' FROM tbAluno
@@ -47,8 +42,7 @@ SELECT nomeAluno AS 'ALUNO', nomeCurso AS 'CURSO' FROM tbAluno, tbMatricula, tbT
 		AND dataMatricula = (SELECT MAX(dataMatricula) FROM tbMatricula)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 06
--- Criar uma consulta que retorne o nome do primeiro aluno a ser matriculado na escola de Idiomas.
+-- 06) Criar uma consulta que retorne o nome do primeiro aluno a ser matriculado na escola de Idiomas.
 
 -- com INNER JOIN:
 SELECT nomeAluno AS 'ALUNO' FROM tbAluno
@@ -61,8 +55,7 @@ SELECT nomeAluno AS 'ALUNO' FROM tbAluno, tbMatricula
 		AND dataMatricula = (SELECT MIN(dataMatricula) FROM tbMatricula)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------
--- exercicio 07
--- Criar uma consulta que retorne o nome, rg e data de nascimento de todos os alunos que estejam matriculados no curso de inglês.
+-- 07) Criar uma consulta que retorne o nome, rg e data de nascimento de todos os alunos que estejam matriculados no curso de inglês.
 
 -- com INNER JOIN:
 SELECT nomeAluno AS 'ALUNO', rgAluno AS 'RG', dataNascAluno AS 'NASCIMENTO' FROM tbAluno
